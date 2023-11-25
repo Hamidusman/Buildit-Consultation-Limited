b314c5bb65bd29e379e15ed20f3f88d93899bfe8
hamid <abdulhamidusman218.gmail.com>
2023-11-25 13:00:14 +0000

    gallery display

--- buildit/settings.py
+++ buildit/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u2&!%&ja#+go)sk0s)(&%$$=(@!7si2_0n%fcrfc_jtjvzv(^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['buildit-consultation-limited-7b79cb2aa64e.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
